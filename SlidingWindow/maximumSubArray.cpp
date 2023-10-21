ecd8963789769db32b3ad04391441dd109fdf1d1
valjor98 <valjor98@gmail.com>
2023-10-21 12:18:29 -0600

    Initial maximum sub array problem

---
+++ SlidingWindow/maximumSubArray.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+// O(n)
+float findMaxAverage_SW(vector<int>& nums, int k){
+    double Average = -INFINITY; // Sets to negative infinity
+    double count = 0;
+
+    for (int i = 0; i < nums.size(); i++) {
+        count += nums[i];
+        if (i >= k) count -= nums[i - k];
+        if (i+1 >= k) Average = max(count, Average);
+    }
+    return Average / double(k);
+}
+
+int main() {
+    int k = 4;
+    vector<int> nums = {1, 12, -5, -6, 50, 3};
+    double answer = findMaxAverage_SW(nums, k);
+    cout << "The maximum average is: " << answer << endl;
+    
+    return 0;
+}
