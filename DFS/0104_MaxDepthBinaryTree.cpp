043945ebcd2a518b042d9acb987156109c13f7a6
valjor98 <valjor98@gmail.com>
2023-11-03 12:19:45 -0600

    Did 104. Maximum Depth of Binary Tree

---
+++ DFS/0104_MaxDepthBinaryTree.cpp
@@ -0,0 +1,32 @@
+// 104. Maximum Depth of Binary Tree
+// DST, Recursivity
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <map>
+#include <unordered_set>
+#include <stack>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+int maxDepth(TreeNode* root) {
+    if(root == NULL){
+        return 0;
+    }
+    else{
+        return 1 + max(maxDepth(root->left), maxDepth(root->right));
+    }
+}
